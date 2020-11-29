3153b03a3ed0d9bdfe2c40897e7118b9015eea5d
Adarsh Prakash <geekguyadarsh@gmail.com>
2020-11-29 16:17:25 +0530

    Updates

---
+++ precache-manifest.7fb6fe27933dc162094ab7186a5d9e92.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbe6476c5e90017cb14dfb05f7a5586",
+    "url": "/burjua-ui/index.html"
+  },
+  {
+    "revision": "5124d0308ae576378e1f",
+    "url": "/burjua-ui/static/css/main.2bb03b5d.chunk.css"
+  },
+  {
+    "revision": "46552334b996cab6e240",
+    "url": "/burjua-ui/static/js/2.6a232085.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/burjua-ui/static/js/2.6a232085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5124d0308ae576378e1f",
+    "url": "/burjua-ui/static/js/main.af933ca8.chunk.js"
+  },
+  {
+    "revision": "6f2279975a7b46c09f26",
+    "url": "/burjua-ui/static/js/runtime-main.7a662478.js"
+  },
+  {
+    "revision": "6e0f36e18e090e8eaee8fc7c291ec8e4",
+    "url": "/burjua-ui/static/media/Cart-black.6e0f36e1.svg"
+  },
+  {
+    "revision": "6426d8dec8b4cf7044f855a237e4ebee",
+    "url": "/burjua-ui/static/media/Cart-grey.6426d8de.svg"
+  },
+  {
+    "revision": "eace30b42191c464b4ea26f2465b53b4",
+    "url": "/burjua-ui/static/media/Cart.eace30b4.svg"
+  },
+  {
+    "revision": "08ebf4f82f897c9d0f72920f9494fa14",
+    "url": "/burjua-ui/static/media/FortuneCookieNF.08ebf4f8.otf"
+  },
+  {
+    "revision": "bdc1fd7c2a35112242865c525d7c7975",
+    "url": "/burjua-ui/static/media/PlayfairDisplay.bdc1fd7c.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/burjua-ui/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "ee6429ccc43a401a607e78ec7b90de0c",
+    "url": "/burjua-ui/static/media/Product.ee6429cc.png"
+  },
+  {
+    "revision": "257d6545dd080d8c205d63f256d41495",
+    "url": "/burjua-ui/static/media/Product1.257d6545.png"
+  },
+  {
+    "revision": "e3fb2a7f399de20ea1ba73f2c394f79d",
+    "url": "/burjua-ui/static/media/Product2.e3fb2a7f.png"
+  },
+  {
+    "revision": "77ad2dd5e21a2d381315abf08913b3d0",
+    "url": "/burjua-ui/static/media/Product3.77ad2dd5.png"
+  },
+  {
+    "revision": "ba979eaade4fdb150c33a8d3cdbf550a",
+    "url": "/burjua-ui/static/media/Search-black.ba979eaa.svg"
+  },
+  {
+    "revision": "8549fe7303e66cec88669ded25e61751",
+    "url": "/burjua-ui/static/media/Search-grey.8549fe73.svg"
+  },
+  {
+    "revision": "81f3b130ae49c8058ee5fba7c1eab808",
+    "url": "/burjua-ui/static/media/Search.81f3b130.svg"
+  },
+  {
+    "revision": "39c0698e5910662d6a20e13846306cb3",
+    "url": "/burjua-ui/static/media/btnArrow.39c0698e.svg"
+  },
+  {
+    "revision": "bcff38952eea8c0d5c97960ccb959c2c",
+    "url": "/burjua-ui/static/media/btnArrowLeft.bcff3895.svg"
+  },
+  {
+    "revision": "f94caed9aac7c02d03b93f01bc7edf0b",
+    "url": "/burjua-ui/static/media/cart-white.f94caed9.svg"
+  },
+  {
+    "revision": "e97f2394cdd0e96ca49a0b0a0f7d6f24",
+    "url": "/burjua-ui/static/media/dropdown-icon.e97f2394.svg"
+  },
+  {
+    "revision": "360ffa786f0515453b52e858a17a31e6",
+    "url": "/burjua-ui/static/media/express-delivery.360ffa78.svg"
+  },
+  {
+    "revision": "167005c3bf6bb0625ac91c3b386d54ae",
+    "url": "/burjua-ui/static/media/external-link-icon.167005c3.svg"
+  },
+  {
+    "revision": "4db47ad0cdfad19204b1a2a53c2767ad",
+    "url": "/burjua-ui/static/media/filterSortIcon.4db47ad0.svg"
+  },
+  {
+    "revision": "714ebdf0675aad822a08e4f20427c419",
+    "url": "/burjua-ui/static/media/image1.714ebdf0.png"
+  },
+  {
+    "revision": "fe3401c87669a156675a6fbbede146b2",
+    "url": "/burjua-ui/static/media/image2.fe3401c8.png"
+  },
+  {
+    "revision": "70228a584c3f7bb0e5da61960ede08f2",
+    "url": "/burjua-ui/static/media/image3.70228a58.png"
+  },
+  {
+    "revision": "7ade0281409c23a06b0b5fbbbf3fa1ab",
+    "url": "/burjua-ui/static/media/shipment-icon.7ade0281.svg"
+  },
+  {
+    "revision": "684542d4a9693de1231cd8308bcd4986",
+    "url": "/burjua-ui/static/media/shopping-bag.684542d4.svg"
+  },
+  {
+    "revision": "8eb34e596368da6615089bc7f3a5b396",
+    "url": "/burjua-ui/static/media/sliderImg.8eb34e59.png"
+  }
+]);
